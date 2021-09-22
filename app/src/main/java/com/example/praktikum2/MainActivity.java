e68f5f74c043c9d9f3acd5919ec44cf40381073a
Your Name <you@example.com>
2021-09-22 20:32:18 +0700

    belajar praktikum 2

---
+++ app/src/main/java/com/example/praktikum2/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.praktikum2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buka_profile;
+    private Button buka_browser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        buka_profile = (Button)findViewById(R.id.btn_buka_profile);
+        buka_profile.setOnClickListener((View.OnClickListener) this);
+
+        buka_browser = (Button)findViewById(R.id.btn_buka_profile_browser);
+        buka_browser.setOnClickListener((View.OnClickListener) this);
+    }
+
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()) {
+
+            case R.id.btn_buka_profile:
+                Intent pp = new Intent(MainActivity.this, ProfileActivity_baru.class);
+                Bundle bd = new Bundle();
+                bd.putString("name", "Gading Sectio Aryoseto");
+                bd.putString("email", "gadingsectio@gmail.com");
+                bd.putInt("age", 19);
+                pp.putExtra("Profile", bd);
+                startActivity(pp);
+                break;
+            case R.id.btn_buka_profile_browser:
+                Intent ff = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com"));
+                Toast.makeText(getApplicationContext(),ff.toString(),Toast.LENGTH_SHORT).show();
+                startActivity(ff);
+                break;
+
+
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/praktikum2/ProfileActivity.java
@@ -0,0 +1,24 @@
+package com.example.praktikum2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile2);
+
+        TextView ff = (TextView)findViewById(R.id.buka_profile);
+        Bundle bundle = getIntent().getBundleExtra("Profile");
+        String name = bundle.getString("name");
+        String email = bundle.getString("email");
+        int age = bundle.getInt("age");
+
+        String text = "Name : " + name +"\n" + "Email : " +email+"\n" + "Age : "
+                +age; ff.setText(text);
+
+    }
+}
